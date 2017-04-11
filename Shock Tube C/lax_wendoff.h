f790363453084794a6503970e94a1598601a543c
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2017-04-11 03:08:36 -0500

    Corrección de métodos para evitar un memory leak

--- Shock Tube C/lax_wendoff.h
+++ Shock Tube C/lax_wendoff.h
@@ -9,10 +9,10 @@
 #define LAX_WENDOFF_H_
 
 u* init(void);
-f* F(u* U_n);
-u* step(u* U_n, f* F_n, double dt);
+void F(f* F_n, u* U_n);
 double u_max(u* U);
 u* lax_wendoff(double tmax, int pI);
+f* f_malloc(void);
 u* u_malloc(void);
 
 #endif /* LAX_WENDOFF_H_ */
