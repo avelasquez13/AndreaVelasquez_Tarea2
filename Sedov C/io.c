f18f9510eb749643195fe8edbcef74b50ad54033
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2017-04-14 12:57:50 -0500

    Define e implementa print_list para exportar datos

--- Sedov C/io.c
+++ Sedov C/io.c
@@ -1,8 +1,22 @@
 #include "struct.h"
 #include <stdio.h>
 
+#define fname "sedov.dat"
+
 void print_L(physics_grid *G){
   fprintf(stdout, "L_x = %f\n", G->L_x);  
   fprintf(stdout, "L_y = %f\n", G->L_y);
   fprintf(stdout, "L_z = %f\n", G->L_z); 
+}
+
+void print_list(double *list, int length){
+  int i;
+  FILE *fp;
+  fp = fopen(fname, "a");
+  
+  for (i=0;i<length;i++){
+    fprintf(fp,"%f ",list[i]);
+  }
+  fprintf(fp,"\n");
+  fclose(fp);
 }
--- Sedov C/io.h
+++ Sedov C/io.h
@@ -1,1 +1,3 @@
 void print_L(physics_grid *G);
+void print_list(double *list, int length);
+
