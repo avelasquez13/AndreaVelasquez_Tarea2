2603c29632d4435b754bee77cd14cbf80d0987a2
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2017-04-14 11:51:11 -0500

    Corrección definición radioChoque

--- Sedov C/fvm.h
+++ Sedov C/fvm.h
@@ -12,13 +12,13 @@ void evolve(physics_grid *P, U_grid *U, F_grid *Fp, F_grid *Fm, double r_final, 
 double step(physics_grid *P, U_grid *U, F_grid *Fp, F_grid *Fm, double tiempo);
 void actualizarP(physics_grid *P, U_grid *U);
 void actualizarF(U_grid *U, F_grid *Fp, F_grid *Fm);
-double radioChoque(physics_grid *P);
+double radioChoque(physics_grid *P, double *radios, int *posiciones);
 void h(U_grid *U, double* h);
 void cs(U_grid *U, double* cs);
 double vmax(physics_grid *P, U_grid *U);
 double dt(physics_grid *P, U_grid *U);
 void propiedad(physics_grid *P, int propiedad, double *prop);
 double presion(double *u_cell);
-void perfilRadial(physics_grid *P, double *radios, double *posiciones, double *dens, double *pres);
+void perfilRadial(physics_grid *P, double *radios, int *posiciones, double *dens, double *pres, double *dist);
 
 #endif /* FVM_H_ */
