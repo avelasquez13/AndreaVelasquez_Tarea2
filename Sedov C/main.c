3b2e8966c5202932aad45425309c1f1368afde2c
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2017-04-15 10:25:05 -0500

    Correcciones: Inicializa tiempo en 0 para que avance lo que retorna evolve (tiempo += evolve), length toma el valor dado por init_radios

--- Sedov C/main.c
+++ Sedov C/main.c
@@ -6,15 +6,15 @@
 
 
 int main(int argc, char **argv){
-	fprintf(stdout, "Sedov: \n");
+  fprintf(stdout, "Sedov: \n");
   physics_grid * P_state;
   U_grid * U_state;
   F_grid  *Fp, *Fm;
   double *radios; // radios[i] = radio de la celda en posiciones[i]
   double *dist, *rho; // Guardan valores de distancia (sin repetir) y promedio de rho a esa distancia
-  double tiempo;
+  double tiempo = 0;
   int *posiciones; // Guarda las posiciones ordenadas ascendentemente por radio
-  int length=0; // Tamaño del SET de radios
+  int length; // Tamaño del SET de radios
   int i;
 
   P_state = create_physics_grid();
@@ -28,7 +28,7 @@ int main(int argc, char **argv){
   rho=create_listNdoubles(P_state);
   posiciones=create_listNints(P_state);
   printf("Size arrays: %d\n",P_state->N_cells);
-  init_radios(P_state, radios, dist, rho, posiciones, length);
+  length = init_radios(P_state, radios, dist, rho, posiciones);
   /*
   init_conditions(U_state,P_state);
   fprintf(stdout, "Inicializo \n");
@@ -37,7 +37,7 @@ int main(int argc, char **argv){
   //Imprime los valores de rho para las posiciones de la onda de choque 10m,60m, y 120m
   double pos[3]={10,60,120};
   for (i=0;i<3;i++){
-	  tiempo = evolve(P_state, U_state, Fp, Fm, pos[i], radios, rho, dist, posiciones, length);
+	  tiempo += evolve(P_state, U_state, Fp, Fm, pos[i], radios, rho, dist, posiciones, length);
 	  print_list(rho,length);
 	  print_time(tiempo);
   }
