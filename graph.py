0065691a6d63f9344be0c56cf2d53ea203230a65
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-04-19 19:35:14 -0500

    fix tabs again

--- graph.py
+++ graph.py
@@ -81,48 +81,48 @@ if sys.argv[1] == "shock":
 	    return val
 
 	def SolucionExacta():
-	    p_c_antes = 0.3
-	    n = 0
-	    p_c = p_c_antes - fP(p_c_antes)/fP_prima(p_c_antes)
-	    var = abs(fP(p_c))
+		p_c_antes = 0.3
+	    	n = 0
+	    	p_c = p_c_antes - fP(p_c_antes)/fP_prima(p_c_antes)
+	    	var = abs(fP(p_c))
 	    
-	    while (var > 1*(10.0**(-10))):
-                    p_c_antes = p_c
-                    p_c = p_c_antes - fP(p_c_antes)/fP_prima(p_c_antes)
-                    var = abs(fP(p_c))
-                    n += 1
+	    	while (var > 1*(10.0**(-10))):
+			p_c_antes = p_c
+                    	p_c = p_c_antes - fP(p_c_antes)/fP_prima(p_c_antes)
+                    	var = abs(fP(p_c))
+                    	n += 1
 		
-	    u_c = 0.5*(u_l + u_r) + 0.5*(fR(p_c) - fL(p_c))
-	    rho_c_l = rho_l*((p_c/P_l)**(1/gamma))
-	    rho_c_r = rho_r*((p_c/P_r) + (gamma - 1)/(gamma + 1))/((gamma - 1)*p_c/(P_r*(gamma + 1)) + 1)
-	    a_c_l = a_l*((p_c/P_l)**(0.5*(gamma - 1)/gamma))
-	    S_h_l = u_l - a_l
-	    S_t_l = u_c - a_c_l
-	    S_r = u_r + a_r*np.sqrt(0.5*(gamma + 1)*p_c/(gamma*P_r) + 0.5*(gamma - 1)/gamma)
+	    	u_c = 0.5*(u_l + u_r) + 0.5*(fR(p_c) - fL(p_c))
+	    	rho_c_l = rho_l*((p_c/P_l)**(1/gamma))
+	    	rho_c_r = rho_r*((p_c/P_r) + (gamma - 1)/(gamma + 1))/((gamma - 1)*p_c/(P_r*(gamma + 1)) + 1)
+	    	a_c_l = a_l*((p_c/P_l)**(0.5*(gamma - 1)/gamma))
+	    	S_h_l = u_l - a_l
+	    	S_t_l = u_c - a_c_l
+	    	S_r = u_r + a_r*np.sqrt(0.5*(gamma + 1)*p_c/(gamma*P_r) + 0.5*(gamma - 1)/gamma)
 
-	    x = np.linspace(0,1,100)
-	    t = 0.21
-	    delta_t = 0.0001
-	    ud = 0.5
-	    RHO = x.copy()
-	    U = x.copy()
-	    PR = x.copy()
+	    	x = np.linspace(0,1,100)
+	    	t = 0.21
+	    	delta_t = 0.0001
+	    	ud = 0.5
+	    	RHO = x.copy()
+	    	U = x.copy()
+	    	PR = x.copy()
 
-	    der_u_max = 0
-	    while (ud < 0.9):
-		t += delta_t
+	    	der_u_max = 0
+	    	while (ud < 0.9):
+			t += delta_t
 		
-		for i in range(len(x)):
-			r,u,p = Reg(x[i],t,rho_c_l,rho_c_r,u_c,p_c,S_h_l,S_t_l,S_r)
-			RHO[i] = r
-		    	U[i] = u
-		    	PR[i] = p
-		    	der_u = abs(U[i] - U[i-1])
-		    	if (der_u_max < der_u):
-		        	ud = x[i]
-		        	der_u_max = der_u
-			der_u_max = 0    
-	    return RHO, U, PR
+                   	for i in range(len(x)):
+				r,u,p = Reg(x[i],t,rho_c_l,rho_c_r,u_c,p_c,S_h_l,S_t_l,S_r)
+                            	RHO[i] = r
+                            	U[i] = u
+                            	PR[i] = p
+                            	der_u = abs(U[i] - U[i-1])
+                            	if (der_u_max < der_u):
+					ud = x[i]
+                                    	der_u_max = der_u
+			    	der_u_max = 0    
+		return RHO, U, PR
 
 	def Reg(x,t,rho_c_l,rho_c_r,u_c,p_c,S_h_l,S_t_l,S_r): # Devuelve rho, u, P
 	    if (x - xdiscont < S_h_l*t): # L
